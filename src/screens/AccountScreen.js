f585995fe5aeb6beb6dfbb50eef239ce14550918
Rodolpho Lima <rodolphovl@icloud.com>
2021-08-01 15:04:37 +0100

    Sign Out Complete

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
+import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const AccountScreen = () => {
+  const { signout } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
-    </View>
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text style={{ fontSize: 48 }}>Account</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
+    </SafeAreaView>
   );
 };
 
